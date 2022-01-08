f891476f4960c7767e7276f1e8379cf0d5aebfc1
Po Yu Chen <pyshif@gmail.com>
2022-01-08 14:44:07 +0800

    add callback1

---
+++ callback/callback1.js
@@ -0,0 +1,37 @@
+// 刷牙 2000
+setTimeout(function () {
+    let current = null;
+    do {
+        current = new Date();
+    } while (current - start < 2000);
+
+    console.log(`刷牙完成: ${current - start} ms`);
+    start = current;
+});
+
+// 吃早餐 3000
+setTimeout(function () {
+    let current = null;
+
+    do {
+        current = new Date();
+    } while (current - start < 3000);
+
+    console.log(`吃完早餐: ${current - start} ms`);
+    start = current;
+});
+
+// 寫功課 2000
+setTimeout(function () {
+    let current = null;
+
+    do {
+        current = new Date();
+    } while (current - start < 2000);
+
+    console.log(`寫完功課: ${current - start} ms`);
+    start = current;
+});
+
+let start = new Date();
+console.log(`開始: ${start}`);
