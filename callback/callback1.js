eb09b37f7b7b1f2975708e479dc6c689fda4063a
Po Yu Chen <pyshif@gmail.com>
2022-01-08 15:17:21 +0800

    fix callback1.js

--- callback/callback1.js
+++ callback/callback1.js
@@ -1,37 +1,47 @@
 // 刷牙 2000
-setTimeout(function () {
-    let current = null;
-    do {
-        current = new Date();
-    } while (current - start < 2000);
+// setTimeout(function () {
+//     let current = null;
+//     do {
+//         current = new Date();
+//     } while (current - start < 2000);
 
-    console.log(`刷牙完成: ${current - start} ms`);
-    start = current;
-});
+//     console.log(`刷牙完成: ${current - start} ms`);
+//     start = current;
+// });
 
 // 吃早餐 3000
-setTimeout(function () {
-    let current = null;
+// setTimeout(function () {
+//     let current = null;
 
-    do {
-        current = new Date();
-    } while (current - start < 3000);
+//     do {
+//         current = new Date();
+//     } while (current - start < 3000);
 
-    console.log(`吃完早餐: ${current - start} ms`);
-    start = current;
-});
+//     console.log(`吃完早餐: ${current - start} ms`);
+//     start = current;
+// });
 
 // 寫功課 2000
+// setTimeout(function () {
+//     let current = null;
+
+//     do {
+//         current = new Date();
+//     } while (current - start < 2000);
+
+//     console.log(`寫完功課: ${current - start} ms`);
+//     start = current;
+// });
+
+// let start = new Date();
+// console.log(`開始: ${start}`);
+
 setTimeout(function () {
-    let current = null;
-
-    do {
-        current = new Date();
-    } while (current - start < 2000);
-
-    console.log(`寫完功課: ${current - start} ms`);
-    start = current;
-});
-
-let start = new Date();
-console.log(`開始: ${start}`);
+    console.log(`刷牙完成`);
+    setTimeout(function () {
+        console.log(`吃完早餐`);
+        setTimeout(function () {
+            console.log(`寫完功課`);
+        }, 2000);
+    }, 3000);
+}, 2000);
